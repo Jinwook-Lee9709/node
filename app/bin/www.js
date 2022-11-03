c5df38908b7c0a4f7c82395c0cefefa0e75e898e
Nelician <wooki9709@gmail.com>
2022-11-03 22:11:48 +0900

    amount 수정

--- app/bin/www.js
+++ app/bin/www.js
@@ -4,14 +4,15 @@ const Cafe = require("../src/models/Cafe");
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () =>{
-    // const rule = new schedule.RecurrenceRule();
-    // rule.hour = 21;
-    // rule.tz = 'Asia/Seoul';
-    // const job = schedule.scheduleJob(rule, async function(){
-    //     const cafe = new Cafe();
-    //     const response = await cafe.safe_stock_update();
-    //     console.log("코드실행");
+    const rule = new schedule.RecurrenceRule();
+    rule.hour = 22;
+    rule.tz = 'Asia/Seoul';
+    const job = schedule.scheduleJob(rule, async function(){
+        const cafe = new Cafe();
+        const response = await cafe.safe_stock_update();
+        console.log("코드실행");
+        console.log(response);
 
-    // });
+    });
     console.log("서버 가동");
 });
