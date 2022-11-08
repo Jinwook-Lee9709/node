0d495f038b558d0dfb8a6d97f1c523305607a97d
Nelician <wooki9709@gmail.com>
2022-11-08 17:18:29 +0900

    수정

--- app/bin/www.js
+++ app/bin/www.js
@@ -13,5 +13,12 @@ app.listen(PORT, () =>{
         const response2 = await cafe.stock_update();
         const response1 = await cafe.safe_stock_update();
     });
+    const rule1 = new schedule.RecurrenceRule();
+    rule1.minute = 0;
+    rule1.tz = 'Asia/Seoul';
+    const job1 = schedule.scheduleJob(rule1, async function(){
+        const cafe = new Cafe();
+        const response2 = await cafe.stock_update();
+    });
     console.log("서버 가동");
 });
